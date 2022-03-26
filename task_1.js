21d92c4c81dddf6320c1678377d513c5c7041655
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2022-03-26 11:51:24 +0300

    add task_1

---
+++ task_1.js
@@ -0,0 +1,24 @@
+// 1 задание
+
+function TF (TC_1) {
+	var TF_1 = (9 / 5) * TC_1 + 32;
+	return TF_1;
+}
+
+var z = TF(25);
+console.log(z)
+
+
+// 2 задание
+var Admin = "Василий";
+var Name = Admin;
+console.log(Name);
+
+// 3 задание
+
+function summ (a, b) {
+	return a + b
+}
+
+var n = summ(1000, "108");
+console.log(n);
